54e9a452c802400f901cece2590827a709f398a3
이수지 <leesuji1998@naver.com>
2025-08-15 19:34:05 +0900

    Completed section03

---
+++ section03/src/index.js
@@ -0,0 +1,9 @@
+import mul, { add, sub } from "./math.js";
+import randomColor from "randomcolor";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(1, 2));
+
+const color = randomColor();
+console.log(color);
