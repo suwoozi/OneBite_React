99ecfa584b406e44aebe94cdd44fcb36c5154da3
이수지 <leesuji1998@naver.com>
2025-08-16 19:18:37 +0900

    Completed section05

---
+++ section05/src/components/Register.jsx
@@ -0,0 +1,71 @@
+import { useState, useRef } from "react";
+
+// 간단한 회원가입 폼
+// 1. 이름
+// 2. 생년월일
+// 3. 국적
+// 4. 자기소개
+
+const Register = () => {
+  const [input, setInput] = useState({
+    name: "",
+    birth: "",
+    country: "",
+    bio: "",
+  });
+
+  const countRef = useRef(0);
+  const inputRef = useRef();
+
+  const onChange = (e) => {
+    countRef.current++;
+
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 DOM 요소에 포커스
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          ref={inputRef}
+          name="name"
+          value={input.name}
+          onChange={onChange}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          name="birth"
+          value={input.birth}
+          onChange={onChange}
+          type="date"
+        />
+      </div>
+      <div>
+        <select name="country" value={input.country} onChange={onChange}>
+          <option></option>
+          <option value="KR"> 한국 </option>
+          <option value="US"> 미국 </option>
+          <option value="UK"> 영국 </option>
+        </select>
+      </div>
+      <div>
+        <textarea name="bio" value={input.bio} onChange={onChange} />
+      </div>
+      <button onClick={onSubmit}> 제출 </button>
+    </>
+  );
+};
+
+export default Register;
