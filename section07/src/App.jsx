aa9ad17e29f15ddae64a01b7c232c1363d905ce5
이수지 <bigbanggoodz@gmail.com>
2025-08-20 22:32:46 +0900

    Completed section07

---
+++ section07/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+import Even from "./components/Even";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    console.log(`count: ${count}`)
+  }, [count])
+
+  const onClickButton = (value) => {
+    setCount(count + value);
+  }
+
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <input value={input} onChange={(e) => {
+          setInput(e.target.value)
+        }}/>
+      </section>
+      <section>
+        <Viewer count={count} />
+        {count % 2 === 0 ? <Even/> : null}
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
